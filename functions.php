ddee4f8cd18bb78c6077f14e45b63623c3383479
Frederik Neirynck <info@fullstackahead.be>
2025-11-06 15:05:44 +0100

    Verwijder custom functies Evergem

--- functions.php
+++ functions.php
@@ -14,7 +14,6 @@
 	require_once get_stylesheet_directory() . '/functions/mailchimp/functions.php';
 	require_once get_stylesheet_directory() . '/functions/vouchers/functions.php';
 	require_once get_stylesheet_directory() . '/functions/subsites/brugge.php';
-	require_once get_stylesheet_directory() . '/functions/subsites/evergem.php';
 	require_once get_stylesheet_directory() . '/functions/subsites/hoeilaart.php';
 	require_once get_stylesheet_directory() . '/functions/subsites/vilvoorde.php';
 	
